2063ba4cb30f585d0c4d79de7983950255065743
docterstrang <49855055+docterstrang@users.noreply.github.com>
2019-05-10 13:20:46 +0500

    Update SJF (1).java

--- SJF (1).java
+++ SJF (1).java
@@ -4,64 +4,64 @@ class SJF
 public static void main(String args[]) 
 {  
 Scanner sc=new Scanner(System.in); 
-int n,BT[],WT[],TAT[], OUT[]; 
+int n,B[],W[],T[], O[]; 
 System.out.println("Enter no of process"); 
 n=sc.nextInt(); 
-BT=new int[n+1]; 
-WT=new int[n+1]; 
-TAT=new int[n+1];
-OUT=new int[n+1];
-float AWT=0;
-float ATAT=0;
+B=new int[n+1]; 
+W=new int[n+1]; 
+T=new int[n+1];
+O=new int[n+1];
+float A=0;
+float AT=0;
 System.out.println("Enter Burst time for each process"); 
 for(int i=0;i<n;i++) 
 { 
 System.out.println("Enter BT for process "+(i+1)); 
-BT[i]=sc.nextInt(); 
-OUT[i]=i+1;
+B[i]=sc.nextInt(); 
+O[i]=i+1;
 }for(int i=0;i<n;i++) 
 {
-WT[i]=0; TAT[i]=0; 
+W[i]=0; T[i]=0; 
 } 
 int temp; 
 for(int i=0;i<n-1;i++) 
 {
 for(int j=0;j<n-1;j++) 
 { 
-if(BT[j]>BT[j+1])    
+if(B[j]>B[j+1])    
 {
-temp=BT[j]; 
-BT[j]=BT[j+1]; 
-BT[j+1]=temp; 
-temp=OUT[j]; 
-OUT[j]=OUT[j+1]; 
-OUT[j+1]=temp; 
+temp=B[j]; 
+B[j]=B[j+1]; 
+B[j+1]=temp; 
+temp=O[j]; 
+O[j]=O[j+1]; 
+O[j+1]=temp; 
 }}
 } 
 for(int i=0;i<n;i++) 
 {
-    TAT[i]=BT[i]+WT[i]; 
-    WT[i+1]=TAT[i]; 
+    T[i]=B[i]+W[i]; 
+    W[i+1]=T[i]; 
 } 
-TAT[n]=WT[n]+BT[n]; 
+T[n]=W[n]+B[n]; 
 for(int j=0;j<n;j++)
  {
-    TAT[j]=WT[j]+BT[j];
-    AWT+=WT[j]; 
+    T[j]=W[j]+B[j];
+    A+=W[j]; 
 }
 System.out.println("  PROCESS   BT      WT      TAT     "); 
 for(int i=0;i<n;i++) 
-System.out.println("    "+ OUT[i] + "       "+BT[i]+"       "+WT[i]+"       "+TAT[i]); 
-AWT=AWT/n; 
+System.out.println("    "+ O[i] + "       "+B[i]+"       "+W[i]+"       "+T[i]); 
+A=A/n; 
 System.out.println("***********************************************"); 
-System.out.println("Avg waiting time="+AWT+"\n***********************************************"); 
+System.out.println("Avg waiting time="+A+"\n***********************************************"); 
 for(int i=0;i<n;i++) 
 { 
-TAT[i]=WT[i]+BT[i]; 
-ATAT=ATAT+TAT[i]; 
+T[i]=W[i]+B[i]; 
+AT=AT+T[i]; 
 }
-ATAT=ATAT/n; 
+AT=AT/n; 
 System.out.println("***********************************************"); 
-System.out.println("Avg turn around time="+ATAT+"\n***********************************************"); 
+System.out.println("Avg turn around time="+AT+"\n***********************************************"); 
 } 
 }
